035d8dbac4e3f0cb3de1981883ef57b50b1fcf4c
Creativious <timothyu120@gmail.com>
2024-01-07 00:19:42 -0500

    Remove tests or program won't compile

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,4 @@
 import net.creativious.SharedMemoryWrapper;
 
 public class Test {
-
-    @org.junit.jupiter.api.Test
-    public void testExample() {
-        var shm = SharedMemoryWrapper.open("test", 1024);
-        String message = shm.read_string();
-        System.out.println(message);
-    }
 }
