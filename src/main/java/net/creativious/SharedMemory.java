e9b0f343c3cb87d2a02043591511a39fbb71c40b
Creativious <timothyu120@gmail.com>
2024-01-06 22:11:55 -0500

    aaa

--- src/main/java/net/creativious/SharedMemory.java
+++ src/main/java/net/creativious/SharedMemory.java
@@ -129,7 +129,7 @@ public class SharedMemory {
                 mode |= 0x00000200 | 0x00000800;
             }
             int fd = (int) shm_open_linux.invokeExact(
-                    (Addressable) MemorySession.global().allocateUtf8String(name).address(),
+                    (MemorySegment) MemorySession.global().allocateUtf8String(name),
                     mode,
                     (int) ((short) 00400 | 00200)
             );
